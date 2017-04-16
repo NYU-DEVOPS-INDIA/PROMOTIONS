7a9680c1765d40555c1e29eacf155d5d54c35df8
Vinayak Raghupathy <raghupathyvinayak@gmail.com>
2017-04-16 23:54:17 +0000

    fixes #75

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -19,18 +19,52 @@ def step_impl(context, message):
 def step_impl(context):
     assert context.resp.data is ""
 
+@then(u'I should see a promotion called "{name}" that is a "{kind}" and has status "{status}"')
+def step_impl(context,name,kind,status):
+    assert name in context.resp.data
+    assert kind in context.resp.data    
+    assert status in context.resp.data    
+
+@then(u'I should not see a promotion that is a "{kind}" and has status "{status}"')
+def step_impl(context,kind,status):
+    assert kind not in context.resp.data    
+    assert status not in context.resp.data     
+
+
 @given(u'the following promotions')
 def step_impl(context):
     server.data_reset()
     url = '/promotions'
     for row in context.table:
-        promotion = {'name': row['name'], 'kind': row ['kind'], 'description': row['description']}
+        promotion = {'name': row['name'], 'kind': row['kind'], 'description': row['description']}
         context.resp = context.app.post(url, data=json.dumps(promotion), content_type='application/json')
+
+@given(u'I create a promotion called "{name}" And I describe it as "{description}" And I set the kind to "{kind}"')
+def step_impl(context,name,description,kind):
+    server.data_reset()
+    url = '/promotions'
+    promotion = {'name': name, 'kind': kind, 'description': description}
+    context.resp = context.app.post(url, data=json.dumps(promotion), content_type='application/json')
+    assert context.resp.status_code == 201
+
+@given(u'I create a promotion with no name And I describe it as "{description}" And I set the kind to "{kind}"')
+def step_impl(context,description,kind):
+    server.data_reset()
+    url = '/promotions'
+    promotion = {'kind': kind, 'description': description}
+    context.resp = context.app.post(url, data=json.dumps(promotion), content_type='application/json')
+    assert context.resp.status_code == 400
+
 
 @when(u'I visit the "{url}"')
 def step_impl(context, url):
     context.resp = context.app.get(url)
     assert context.resp.status_code == 200
+
+@when(u'I visit the "{url}" with no promotions')
+def step_impl(context, url):
+    context.resp = context.app.get(url)
+    assert context.resp.status_code == 404    
 
 @when(u'I visit the promotion "{url}" with id "{id}"')
 def step_impl(context, url, id):
